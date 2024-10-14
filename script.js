ab579f8ccb12561dffd75180a3fcb65e47ebada6
arshad786pasha <arshad785pasha65@gmail.com>
2024-10-14 19:26:03 +0530

    go

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#main'),
+//     smooth: true
+// }); 
+
+
+var a = document.querySelector("#ele1")
+console.log(a)
