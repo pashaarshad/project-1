b0860a256e76ed61b7c80319a822d89f2661c343
arshad786pasha <arshad785pasha65@gmail.com>
2024-11-22 18:59:18 +0530

    ok ok

--- script.js
+++ script.js
@@ -4,5 +4,55 @@
 // }); 
 
 
-var a = document.querySelector("#ele1")
-console.log(a)
+var ele = document.querySelector(".fix-con")
+var fix = document.querySelector(".fix-img")
+
+
+ele.addEventListener("mouseenter",function(){
+    fix.style.display = "block"
+})
+
+ele.addEventListener("mouseleave",function(){
+    fix.style.display = "none"
+}) 
+
+// var main_con = document.querySelector(".page4-con")
+// main_con.addEventListener("mouseenter",function(){
+//     var img = main_con.getAttribute('data-img')
+//     fix.style.backgroundImage = `url(${img})`
+// })
+
+
+// var main_con = document.querySelectorAll(".page4-con")
+// main_con.forEach(function(e){
+//     e.addEventListener("mouseenter",function(){
+//         var img = main_con.getAttribute("data-img")
+//     fix.style.backgroundImage = `url(${img})`
+//     })
+ 
+
+// })
+
+
+var eles = document.querySelectorAll(".page4-con")
+eles.forEach(function(e){
+    e.addEventListener("mouseenter",function(){
+        var img = e.getAttribute("data-img")
+        fix.style.backgroundImage = `url(${img})`
+    })
+})
+
+
+//   // JavaScript to create 100 stars at random positions
+//   const starsContainer = document.getElementById('starsContainer');
+
+//   for (let i = 0; i < 100; i++) {
+//     const star = document.createElement('div');
+//     star.classList.add('star');
+//     // Randomize position
+//     star.style.top = Math.random() * 100 + 'vh';
+//     star.style.left = Math.random() * 100 + 'vw';
+//     // Add a unique animation delay to each star
+//     star.style.animationDelay = `${Math.random() * 2}s`;
+//     starsContainer.appendChild(star);
+//   }
